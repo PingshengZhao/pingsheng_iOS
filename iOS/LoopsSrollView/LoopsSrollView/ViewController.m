3dfd1f9bb5fb8a8b0ae57d8f24fdf5b33385331e
赵坪生 <18610303170@163.com>
2017-05-11 12:09:57 +0800

    。。。

--- iOS/LoopsSrollView/LoopsSrollView/ViewController.m
+++ iOS/LoopsSrollView/LoopsSrollView/ViewController.m
@@ -19,7 +19,7 @@
     self.title = @"轮播图";
     self.view.backgroundColor = [UIColor whiteColor];
     [self.view addSubview:[PSScrollView psScrollViewWithFrame:CGRectMake(0, 0, kScreenWidth, 150) imagesGroup:@[@"h1", @"h2", @"h3", @"h4"] timeInterval:5 completion:^(NSInteger index) {
-        NSLog(@"你点击了弟%ld张图片...", index);
+        NSLog(@"你点击了弟%ld张图片...    ", index);
     }]];
     }
 
